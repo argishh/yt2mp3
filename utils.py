677961845c457f3001a08d779b6faf0bf3bc8d5c
argishh <abhangiargish@gmail.com>
2024-11-27 20:51:49 +0530

    updated app

---
+++ utils.py
@@ -0,0 +1,28 @@
+FOOTER = """<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #0e1117;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by Argish<br>
+<i>For educational purposes only</i></p>
+</div>
+"""
